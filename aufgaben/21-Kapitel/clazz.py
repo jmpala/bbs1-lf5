3fff96709f1258e568a5c1ba3491f1b1f13ee574
jmpala <juanmartinpalazzolo@icloud.com>
2023-05-08 14:28:59 +0200

    Kapitel 21

---
+++ aufgaben/21-Kapitel/clazz.py
@@ -0,0 +1,8 @@
+from cmath import sqrt
+
+class Clazz:
+    def is_empty_list(self, lista: list) -> bool:
+        return len(lista) == 0
+
+    def square_root(self, value: int) -> complex:
+        return sqrt(value)
---
+++ aufgaben/21-Kapitel/k21u1.py
@@ -0,0 +1,8 @@
+"""
+Aufgabe	K21	|	A1: Erstelle	eine	Klasse	mit	folgenden	Methoden:
+is_empty_list(list)	nimmt	eine	Liste	und	prüft	ob	diese	leer	ist.	Falls	ja,	wird	True	zurückgegeben.
+wurzel(number)	nimmt	eine	Zahl	und	berechnet	die	Wurzel	daraus.
+Erstelle	für	beide	Methoden	jeweils	geeignete	Testfälle	(mindestens	4	pro	Methode).	Jeweils	zwei	Testfall
+für	valide	und	invalide	Eingaben
+"""
+
